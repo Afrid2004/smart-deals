ccd97642298786f76eecea2a46c5ba5365d7754e
afrid2004 <mdfaisalafrid@gmail.com>
2026-05-10 00:02:20 +0600

    added my bids page

--- src/pages/MyBid/index.jsx
+++ src/pages/MyBid/index.jsx
@@ -1,7 +1,170 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { CircleAlert } from "lucide-react";
+import Loading from "../../components/Loading";
+import Swal from "sweetalert2";
 
 const MyBid = () => {
-  return <div>MyBid</div>;
+  const { user } = use(AuthContext);
+  const [bids, setBids] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user?.email) {
+      setLoading(true);
+      fetch(`http://localhost:3000/bids?email=${user?.email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setBids(data);
+          console.log(data);
+        })
+        .catch((err) => console.log(err.message))
+        .finally(() => setLoading(false));
+    }
+  }, [user?.email]);
+
+  const handleRemoveBid = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#009689",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed)
+        fetch(`http://localhost:3000/bids/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your bid has been deleted.",
+                icon: "success",
+              });
+              const filterBid = bids.filter((bid) => bid._id != id);
+              setBids(filterBid);
+            }
+          })
+          .catch((err) => {
+            alert(err.message);
+          });
+    });
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div className="bg-gray-100">
+      <div className="container py-10">
+        <div>
+          <h1 className="font-bold text-3xl mb-5">
+            My Bids: <span className="gradient-text">{bids.length}</span>
+          </h1>
+          {bids.length ? (
+            <div className="bg-white max-h-100 overflow-auto p-5 border border-gray-200 rounded-lg">
+              <table className="min-w-200 w-full">
+                <thead>
+                  <tr className="text-left">
+                    <th className="w-15 pb-3">SL No</th>
+                    <th className="w-62.5 pb-3">Product</th>
+                    <th className="w-62.5 pb-3">Buyer</th>
+                    <th className="w-30 pb-3">Bid Price</th>
+                    <th className="w-50 pb-3">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {bids.map((bid, index) => {
+                    const names = bid?.buyer_name || "User";
+                    const [first, last] = names.split(" ");
+                    const avatar = (first[0] || "") + (last ? last[0] : "");
+
+                    return (
+                      <tr key={index} className="border-t border-gray-300">
+                        <td className="py-5">{index + 1}</td>
+
+                        <td className="py-5">
+                          <div className="flex gap-2 items-center">
+                            <div className="w-20 h-12 shrink-0 overflow-hidden border border-gray-200">
+                              <img
+                                src={bid.productData.image}
+                                className="w-full h-full object-cover"
+                              />
+                            </div>
+                            <div>
+                              <h2 className="font-semibold">
+                                {bid.productData.title}
+                              </h2>
+                              <p className="text-gray-500 whitespace-nowrap">
+                                ${bid.productData.price_min} - $
+                                {bid.productData.price_max}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+
+                        <td className="py-5">
+                          <div className="flex gap-2 items-center">
+                            {bid?.buyer_image ? (
+                              <img
+                                src={bid?.buyer_image}
+                                alt={bid?.buyer_name}
+                                className="w-10 h-10 rounded-full"
+                                title={bid?.buyer_name}
+                              />
+                            ) : (
+                              <div
+                                title={names}
+                                className="w-10 h-10 cursor-default gradient text-white flex items-center justify-center rounded-full"
+                              >
+                                {avatar}
+                              </div>
+                            )}
+                            <div>
+                              <h2 className="font-semibold">
+                                {bid.buyer_name}
+                              </h2>
+                              <p className="text-gray-500 whitespace-nowrap">
+                                {bid.buyer_email}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+
+                        <td className="whitespace-nowrap py-5">
+                          ${bid.bid_price}
+                        </td>
+
+                        <td className="py-5">
+                          <div className="flex gap-2">
+                            <button
+                              onClick={() => handleRemoveBid(bid._id)}
+                              className="px-4 py-1 border border-red-600 text-red-600 hover:bg-red-600 hover:text-white duration-150 cursor-pointer"
+                            >
+                              Remove Bid
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <p className="p-4 rounded-md bg-amber-300/30 text-amber-600 border border-amber-300/50 flex items-center gap-2">
+              <CircleAlert className="w-5 shrink-0" />
+              No bids found for this product.
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyBid;
--- src/pages/Root/Index.jsx
+++ src/pages/Root/Index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "../../components/Footer";
+import Loading from "../../components/Loading";
 
 const Root = () => {
+  const navigate = useNavigation();
+  if (navigate.state == "loading") {
+    return <Loading />;
+  }
   return (
     <>
       <Navbar></Navbar>
